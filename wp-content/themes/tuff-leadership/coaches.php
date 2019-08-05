4920f091e1788d3b94f573b565383d120c766074
Dmitriy Kaminskiy <dima.vampire98@gmail.com>
2019-08-05 14:03:47 +0300

    1. modified coaches.php from repeaters in repeater to one repeater 2. modified styles for coaches.php block for one repeater

--- wp-content/themes/tuff-leadership/coaches.php
+++ wp-content/themes/tuff-leadership/coaches.php
@@ -1,76 +1,45 @@
 <?php $path = get_template_directory_uri(); ?>
 
 <section class="wrapper wrapper_milk" id="coach">
-      <div class="container container_milk">
-        <div class="header">
-          <div class="title">
-            <a href="#top" class="title__box">
-              <img src="<?php echo $path?>/i/tuff.png" class="tuffLogo" alt="" />
-            </a>
-            <div class="registration registration_third">
-              <a href="#registration" class="registration__link">Registration</a>
-            </div>
-          </div>
+  <div class="container container_milk">
+    <div class="header">
+      <div class="title">
+        <a href="#top" class="title__box">
+          <img src="<?php echo $path?>/i/tuff.png" class="tuffLogo" alt="" />
+        </a>
+      <div class="registration registration_third">
+        <a href="#registration" class="registration__link">Registration</a>
+      </div>
+    </div>
 
-          <div>
-            <span class="coaches-title">
-              Coaches <span class="coaches-title__dot"></span>
-            </span>
-          </div>
-        </div>
+      <div>
+        <span class="coaches-title">
+          Coaches <span class="coaches-title__dot"></span>
+        </span>
+      </div>
+  
 
-        <div class="coaches">
+  <div class="coaches">
           <?php
             if( have_rows('coach_repeater') ):
             while ( have_rows('coach_repeater') ) : the_row();
           ?>
 
-            <?php
-              if( have_rows('left_side_repeater') ):
-              while ( have_rows('left_side_repeater') ) : the_row();
-            ?>
               <div class="coaches__item">      
                 <div class="photo">
-                  <img src="<?php the_sub_field('coach_img')?>" alt="one" class="photo__img-one" />
-                  <img src="<?php echo $path?>/i/dots.png" alt="" class="photo__dots">
+                  <img src="<?php the_sub_field('img')?>" alt="" class="photo__img" />
                 </div>
                 <div class="text">
                   <p class="text__name">
-                    <span><embed class="text__snake" src="<?php echo $path?>/i/snake.svg" type=""></span>  
-                    <?php the_sub_field('coach_title'); ?> 
+                    <?php the_sub_field('title'); ?> 
                   </p>
-                  <p class="text__info"> <?php the_sub_field("coach_text"); ?> </p>
+                  <p class="text__info"> <?php the_sub_field('text'); ?> </p>
                   </div>
               </div>
-            <?php 
+          <?php 
             endwhile;
             endif;
-            ?>
-
-            <?php
-              if( have_rows('right_side_repeater') ):
-              while ( have_rows('right_side_repeater') ) : the_row();
-            ?>
-            <div class="coaches__item">
-              <div class="photo photo_right">
-                <img src="<?php the_sub_field('coach_img') ?>" alt="two" class="photo__img-two" />
-                <img src="<?php echo $path?>/i/stripes.png" alt="" class="photo__stripes">
-              </div>
-              <div class="text text_left">
-                <p class="text__name" ><?php the_sub_field('coach_title'); ?></p>
-                <p class="text__info text__info_left"><?php the_sub_field("coach_text"); ?></p>
-              </div>
-            </div>
-            <?php 
-            endwhile;
-            endif;
-            ?>
-
-          <?php
-            endwhile;
-            endif;
-          ?>       
-
-        </div>
-      </div>
-    </section>
+          ?>
+    </div>
+  </div>  
+</section>
